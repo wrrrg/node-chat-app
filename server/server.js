6016d98306f141f6a37c2268ac9e469fc35386b5
wrrrg <bill.gryta@gmail.com>
2018-05-11 19:54:26 -0500

    added socket connection and disconnect listeners

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "../public");
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
@@ -11,6 +15,15 @@ app.use(express.static(publicPath));
 //   res.render("index.html");
 // });
 
-app.listen(PORT, () => {
+//do something when a client connects to the server
+io.on("connection", socket => {
+  console.log("New user connected");
+
+  socket.on("disconnect", socket => {
+    console.log("Client has disconnected");
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
