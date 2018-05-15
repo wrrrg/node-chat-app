a22cabcdb8f2f18cf4bb5f699a3a0a2014908bac
wrrrg <bill.gryta@gmail.com>
2018-05-15 02:57:32 -0500

    scrolling based on user's window

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,42 @@
-var socket = io()
+var socket = io();
+
+function scrollToBottom() {
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect', function() {
-  console.log('Connected to server')
-})
+  console.log('Connected to server');
+});
 
 socket.on('disconnect', function() {
-  console.log('Disconnected from server')
-})
+  console.log('Disconnected from server');
+});
 
 socket.on('newMessage', function(message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a')
-  var template = $('#message-template').html()
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = $('#message-template').html();
   var html = Mustache.render(template, {
     from: 'User',
     text: message.text,
     createdAt: formattedTime
-  })
+  });
 
-  $('#messages').append(html)
+  $('#messages').append(html);
+  scrollToBottom();
 
   // console.log("newMessage", message);
   // var formattedTime = moment(message.createdAt).format("h:mm a");
@@ -26,18 +45,19 @@ socket.on('newMessage', function(message) {
   // li.text(`${message.from} (${formattedTime}): ${message.text}`);
   //
   // $("#messages").append(li);
-})
+});
 
 socket.on('newLocationMessage', function(message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a')
-  var template = $('#location-message-template').html()
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = $('#location-message-template').html();
   var html = Mustache.render(template, {
     from: 'User',
     url: message.url,
     createdAt: formattedTime
-  })
+  });
 
-  $('#messages').append(html)
+  $('#messages').append(html);
+  scrollToBottom();
 
   // var li = $('<li></li>')
   // var a = $("<a target='_blank'>My Location</a>")
@@ -46,12 +66,12 @@ socket.on('newLocationMessage', function(message) {
   // a.attr('href', message.url)
   // li.append(a)
   // $('#messages').append(li)
-})
+});
 
-var messageText = $('[name=message]')
+var messageText = $('[name=message]');
 
 $('#send-message').on('submit', function(e) {
-  e.preventDefault()
+  e.preventDefault();
 
   socket.emit(
     'createMessage',
@@ -60,45 +80,45 @@ $('#send-message').on('submit', function(e) {
       text: messageText.val()
     },
     function() {
-      messageText.val('')
+      messageText.val('');
     }
-  )
-})
+  );
+});
 
-var locationButton = $('#send-location')
+var locationButton = $('#send-location');
 
 locationButton.on('click', function() {
-  locationButton.html('Sending Location')
+  locationButton.html('Sending Location');
 
   if (!navigator.geolocation) {
-    return alert('geolocation not supported')
+    return alert('geolocation not supported');
   } else {
-    locationButton.attr('disabled', 'disabled')
+    locationButton.attr('disabled', 'disabled');
     navigator.geolocation.getCurrentPosition(
       function(position) {
         socket.emit('createLocationMessage', {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude
-        })
-        locationButton.html('Location Sent')
+        });
+        locationButton.html('Location Sent');
 
         var enableLocationButton = function() {
-          locationButton.removeAttr('disabled')
-          locationButton.html('Send Location')
-        }
+          locationButton.removeAttr('disabled');
+          locationButton.html('Send Location');
+        };
 
-        setTimeout(enableLocationButton, 1000)
+        setTimeout(enableLocationButton, 1000);
       },
       function() {
-        alert('unable to fetch location')
+        alert('unable to fetch location');
 
         var enableLocationButton = function() {
-          locationButton.removeAttr('disabled')
-          locationButton.html('Send Location')
-        }
+          locationButton.removeAttr('disabled');
+          locationButton.html('Send Location');
+        };
 
-        setTimeout(enableLocationButton, 1000)
+        setTimeout(enableLocationButton, 1000);
       }
-    )
+    );
   }
-})
+});
